548e6b35bf687bb27cd55c35ce3492610c41c515
Reljod Oreta <oretareljod@gmail.com>
2023-12-27 20:08:51 +0800

    Create overall financial summary

---
+++ src/financial-transactions/overall-summary.js
@@ -0,0 +1,29 @@
+const dv = {}; // mock dataview
+
+// constants
+const TAGS = "[[financial transactions]]";
+const TEMPLATE_FOLDER = "003 - Templates";
+const COLUMNS = { datetime: "datetime", source: "source", type: "type", details: "details", amount: "amount", remarks: "remarks" };
+
+// functions
+const getDatetime = (row) => row[COLUMNS.datetime] ? row[COLUMNS.datetime] : row.file.mtime;
+const mapRow = (row) => {
+    const remarks = row[COLUMNS.remarks] ? row[COLUMNS.remarks] : row.file.name;
+    const datetime = getDatetime(row);
+    const fieldMap = Object.values(COLUMNS).slice(1, -1).map(field => row[field]);
+    return [datetime, ...fieldMap, remarks];
+}
+
+// main code
+if (dv.current().file.folder !== TEMPLATE_FOLDER) {
+    const rows = dv.pages(TAGS)
+        .filter(row => !dv.equal(row[COLUMNS.type], null));
+
+    const typeGroup = rows.groupBy(col => col[COLUMNS.type]).map(val => [val.key, val.rows.values.reduce((acc, s) => acc + s[COLUMNS.amount], 0)]);
+
+    dv.table(["txn type", "amount"], typeGroup);
+    dv.paragraph("\n");
+    dv.table(Object.values(COLUMNS), rows.map(x => mapRow(x)));
+} else {
+    dv.el("p", "Currently on a Template. Table will not render.");
+}
