844024c25a0f8fce988236bce86eda19f83a782e
Reljod Oreta <oretareljod@gmail.com>
2023-12-27 19:52:49 +0800

    Add amount sum per transaction type

--- src/financial-transactions/get-daily.js
+++ src/financial-transactions/get-daily.js
@@ -3,7 +3,7 @@ const dv = {}; // mock dv
 // constants
 const TAGS = "[[financial transactions]]"
 const TEMPLATE_FOLDER = "003 - Templates"
-const COLUMNS = { datetime: "datetime", type: "type", details: "details", amount: "amount", remarks: "remarks" };
+const COLUMNS = { datetime: "datetime", source: "source", type: "type", details: "details", amount: "amount", remarks: "remarks" };
 const fields = Object.values(COLUMNS).slice(1,);
 
 // functions
@@ -17,15 +17,19 @@ const mapRow = (row) => {
 // main code
 if (dv.current().file.folder !== TEMPLATE_FOLDER) {
     const rows = dv.pages(TAGS)
-        .map(x => mapRow(x))
-        .filter(k => !dv.equal(k[Object.values(COLUMNS).findIndex(a => a === COLUMNS.type)], null))
+        .filter(row => !dv.equal(row[COLUMNS.type], null))
         .filter(row => {
-            const today = new Date(row[0]).setHours(0, 0, 0, 0);
+            const datetime = getDatetime(row)
+            const today = new Date(datetime).setHours(0, 0, 0, 0);
             const currentFileDate = dv.current().file.mday.ts;
             return today === currentFileDate;
         });
 
-    dv.table(Object.values(COLUMNS), rows);
+    const typeGroup = rows.groupBy(col => col[COLUMNS.type]).map(val => [val.key, val.rows.values.reduce((acc, s) => acc + s[COLUMNS.amount], 0)]);
+
+    dv.table(["txn type", "amount"], typeGroup);
+    dv.paragraph("\n");
+    dv.table(Object.values(COLUMNS), rows.map(x => mapRow(x)));
 } else {
     dv.el("p", "Currently on a Template. Table will not render.");
 }
